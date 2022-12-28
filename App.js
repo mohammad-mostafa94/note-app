50c6c0010d93bcda4ed4a46cf4eeb137c518d75b
mohammad-mostafa94 <mohammadmostafa1094@gmail.com>
2022-12-28 12:17:59 +0600

    sign in part added

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/home';
+import SignIn from './src/screens/signin';
+import SignUp from './src/screens/signup';
+import Edit from './src/screens/edit';
+import Create from './src/screens/create';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = false; // not authenticated
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Stack.Navigator >
+        {
+          user ? (
+            <>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="Edit" component={Edit} />
+              <Stack.Screen name="Create" component={Create} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="SignIn" component={SignIn} />
+              <Stack.Screen name="SignUp" component={SignUp} />
+            </>
+          )
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
