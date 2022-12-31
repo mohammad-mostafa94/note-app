39bf40bb1f7e77fcc4f77b40cf765d30699154ab
mohammad-mostafa94 <mohammadmostafa1094@gmail.com>
2022-12-31 23:20:25 +0600

    login function is added

--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,10 +1,27 @@
-import { View, Text, Image, TextInput, StyleSheet, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity, TextInput, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../components/Button';
 import Input from '../components/Input';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../App';
 
 export default function SignIn({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigateToSignUp = () => {
+    navigation.navigate("SignUp")
+  }
+
+  const login = () => {
+    signInWithEmailAndPassword(auth, email, password).then((res) => {
+      console.log("Signed in successfully.", res);
+    })
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Image
@@ -15,8 +32,16 @@ export default function SignIn({ navigation }) {
         Never forget your notes.
       </Text>
       <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
-        <Input placeholder="Email address" />
-        <Input placeholder="Password" />
+        <Input
+          placeholder="Email address"
+          onChangeText={(text) => setEmail(text)}
+          autoCapitalize={"none"}
+        />
+        <Input
+          placeholder="password"
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+        />
       </View>
       <View
         style={{
@@ -25,17 +50,27 @@ export default function SignIn({ navigation }) {
           alignItems: "center"
         }}
       >
-        <Button
-          title={"Login"}
-          customStyles={{ alignSelf: "center", marginBottom: 60 }}
-        />
-        <Pressable onPress={() => { navigation.navigate("SignUp") }}>
+
+        {error && <Text style={{ color: "red", marginTop: 10 }}>{error}</Text>}
+
+        {
+          loading ? (
+            <ActivityIndicator />
+          ) : (
+            <Button
+              title={"Login"}
+              customStyles={{ alignSelf: "center", marginBottom: 60 }}
+              onPress={login}
+            />
+          )
+        }
+        <TouchableOpacity onPress={navigateToSignUp}>
           <Text>Don't have an account? {" "}
             <Text
               style={{ color: "green", fontWeight: "bold", marginLeft: 5 }}
             >Sign up</Text>
           </Text>
-        </Pressable>
+        </TouchableOpacity>
 
       </View>
     </SafeAreaView>
